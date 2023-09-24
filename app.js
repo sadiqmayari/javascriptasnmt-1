b4f1e75591a9825f44fd4593fe145a79530d6be7
sadiqmayari <sadiqmayari@gmail.com>
2023-09-24 22:18:33 +0500

    first assigments

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Error! Please enter a valid password.")
+
+var welcomeMessage = "Welcom to JS land...\n Happy Coding!"
+
+alert(welcomeMessage)
+
+var firstMessage = "Welcome to JS Land..."
+var secondMessage = "Happy Coding"
+
+alert(firstMessage)
+alert(secondMessage)
+
+console.log("Hello... I can Run java script in my web browser's console")
